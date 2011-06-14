7c5a42532205673a09aa835a70f12f60fbe1dc6a
MPallas <pallas.mikko@gmail.com>
2011-06-14 13:35:30 +0300

    Added more functions to EasyLaseUsb2Native.
    Change-Id: Ic41c6e02b54314407c8cd335812628473a00d1f5

--- tests/laserschein/tests/TestEasyLaseUsb2.java
+++ tests/laserschein/tests/TestEasyLaseUsb2.java
@@ -1,7 +1,6 @@
 package laserschein.tests;
 
 import com.sun.jna.ptr.IntByReference;
-
 import laserschein.EasylaseUsb2Native;
 
 public class TestEasyLaseUsb2 {
@@ -15,7 +14,18 @@ public class TestEasyLaseUsb2 {
 		IntByReference iref = new IntByReference();
 		iref.setValue(0);
 		
+		EasylaseUsb2Native.PTSTRUCT.ByReference x = new EasylaseUsb2Native.PTSTRUCT.ByReference();
+		
+		x.x = 10;
+		x.y = 10;
+		x.r = 255;
+		x.g = 0;
+		x.b = 0;
+		x.i = 250;
+		
 		System.out.println(EasylaseUsb2Native.EasyLaseGetCardNum());
+		
+		System.out.println(EasylaseUsb2Native.EasyLaseWriteFrame(iref, x, 8, (short)1000));
 		
 		System.out.println(EasylaseUsb2Native.EasyLaseGetStatus(iref));
 		
